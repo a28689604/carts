755d64a556dfee6a49c4dc07889f47077506685b
a28689604 <a28689604@gmail.com>
2023-03-08 15:52:29 +0800

    finish store and slice

---
+++ src/components/CarForm.js
@@ -0,0 +1,10 @@
+function CarForm() {
+  return (
+    <div className="car-form panel">
+      <h4 className="subtitle is-3">Add Car</h4>
+      <form></form>
+    </div>
+  );
+}
+
+export default CarForm;
